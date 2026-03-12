3a588230dcf1c177a2e40145c52c331ce080de01
paniz nourani <paniznourani80@gmail.com>
2026-03-12 23:01:16 +0100

    Fix Update tofixed

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React from "react";
 
 //Style
 import styles from "./Coin.module.css";
 
-const Coin = ({image, marketCap, price, priceChange, symbol, name}) => {
-    return (
-        <div className={styles.container}>
-            <img className={styles.image} src={image} alt={name} />
-            <span className={styles.name}>{name}</span>
-            <span className={styles.symbol}>{symbol.toUpperCase()}</span>
-            <span className={styles.currentPrice}>$ {price.toLocaleString()}</span>
-            <span className={priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange}>{priceChange.toFixed(2)}%</span>
-            <span className={styles.marketCap}>$ {marketCap.toLocaleString()}</span>
-        </div>
-    );
+const Coin = ({ image, marketCap, price, priceChange, symbol, name }) => {
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={image} alt={name} />
+      <span className={styles.name}>{name}</span>
+      <span className={styles.symbol}>{symbol.toUpperCase()}</span>
+      <span className={styles.currentPrice}>
+        $ {price ? price.toLocaleString() : "N/A"}
+      </span>
+      <span
+        className={
+          priceChange !== null && priceChange !== undefined
+            ? priceChange > 0
+              ? styles.greenPriceChange
+              : styles.redPriceChange
+            : styles.redPriceChange
+        }
+      >
+        {priceChange !== null && priceChange !== undefined
+          ? priceChange.toFixed(2)
+          : "N/A"}
+        %
+      </span>
+      <span className={styles.marketCap}>
+        $ {marketCap ? marketCap.toLocaleString() : "N/A"}
+      </span>
+    </div>
+  );
 };
 
 export default Coin;
